a1187c71cf128dbd00678b456dcd237534437e49
Pereira Fraga <eypa@gft.com>
2017-03-01 17:29:36 +0100

    WIP on package.json webpackage.config.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
+import App from "./components/App";
+
+if (process.env.NODE_ENV == "production") {
+	console.warn("The application is in production mode.");
+}
 
 const MOUNT_NODE = document.getElementById("app");
 
 ReactDOM.render(
 	<AppContainer>
-	<App />
+		<App />
 	</AppContainer>,
 	MOUNT_NODE
 );
+
+if (module.hot) {
+	module.hor.accept("./components/App", () => {
+		console.info("Hot Module for index.js");
+		const NewApp = require("./components/App").default;
+		ReactDOM.render(
+			<AppContainer>
+				<NewApp />
+			</AppContainer>, 
+			MOUNT_NODE
+		);
+	});
+}
---
+++ webpack.config.js
@@ -0,0 +1,131 @@
+const webpack = require("webpack");
+
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var CompressionPlugin = require("compression-webpack-plugin");
+var webpackUglifyJslugin = require("webpack-uglify-js-plugin");
+var ZipPlugin = require("zip-webpack-plugin");
+var ShellPlugin = require("webpack-shell-plugin");
+var argv = require("yargs").default({ env: "dev" }).argv;
+
+const PACKAGE = require("./package.json");
+var ENV = argv.env;
+
+const DEPLOY_FILE_NAME = `${PACKAGE.name}-${PACKAGE.version}-${ENV}.zip`;
+
+module.exports = {
+	entry: {
+		js: argv.env == "local" ? ["webpack/hot/only-dev-server", "react-hot-loader/patch", "babel-polyfill", "./app/index.js"] : [ "babel-polyfill","./app/index.js"],
+		vendor: ["react","react-dom","redux","react-redux","es6-promise","react-addons-shallow-compare","react-virtualized"]
+	},
+	devtool: (argv.env=="dev" || argv.env=="local") ? "source-map" : "",
+	devServer: {
+		contentBase: "dist/"
+	},
+	output: {
+		path: __dirname + "/dist",
+		filename: "assets/js/bundle.js"
+	},
+	module: {
+		preLoaders: [
+			{ test: /\.json/, loader: "json" }
+		],
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel",
+				query: {
+					presets: ["es2015", "react", "stage-0"]
+				}
+			},
+			{ test:/\.css$/, loader: "style-loader|css-loader" },
+			{ test:/\.scss$/, loaders: ["style", "css", "sass"] },
+			{ test:/\.less$/, loaders: ["style", "css", "less"] },
+			{ test:/\.gif$/, loader: "url-loader?mimetype=image/jpg" },
+			{ test:/\.png$/, loader: "url-loader?mimetype=image/png" },
+			{ test:/\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+			{ test:/\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=assets/fonts/[name].[ext]" }
+		]
+	},
+	plugins: ( function (argv) {
+		var plugins = [
+			new CleanWebpackPlugin((["dist"])),
+			new webpack.optimize.CommonsChunkPlugin({
+				name: "vendor",
+				minChunks: Infinity,
+				filename: "assets/js/vendor.bundle.js"
+			}),
+			new HtmlWebpackPlugin({
+				template: __dirname + "/app/index.html",
+				filename: "index.html",
+				inject: "body"
+			}),
+			new webpack.NormalModuleReplacementPlugin(/Autosuggest\.scss$/, "react-bootstrap-autosuggest/src/Autosuggest.scss"),
+			new CopyWebpackPlugin([{ from: "app/assets", to: "assets" }]),
+			new CompressionPlugin({test: /\.js$|\.css$|\html$/})
+		];
+
+		if (argv.clean) {
+			console.info("\x1b[36m%s\x1b[0m", ":: Cleaning ::");
+			plugins.push(new CleanWebpackPlugin((["dist"])));
+		}
+
+		if (argv.run) {
+			console.info("\x1b[36m%s\x1b[0m", ":: Run config ::");
+			plugins.push(new webpack.HotModuleReplacementPlugin());
+			plugins.push(new webpack.NoErrorsPlugin());
+		}
+
+		console.warn("\x1b[36m%s\x1b[0m", `Environment: ${argv.env}`);
+
+		switch (argv.env) {
+			case "prod":
+				plugins.push(
+					new webpack.DefinePlugin({
+						'process.env': {
+							NODE_ENV: JSON.stringify("production")
+						},
+						API_URL: JSON.stringify('http://edigley.com/')
+					})
+				);
+				break;
+			case "local":
+				plugins.push(
+					new webpack.DefinePlugin({
+						API_URL: JSON.stringify('http://localhost:9999/')
+					})
+				);
+				break;
+			default:
+				plugins.push(
+					new webpack.DefinePlugin({
+						API_URL: JSON.stringify('http://localhost:9999/')
+					})
+				);			
+				break;
+		}
+
+		if (argv.package) {
+			console.info("\x1b[36m%s\x1b[0m", ":: Packaging ::");
+			plugins.push(
+				new webpackUglifyJslugin({
+					cacheFolder: __dirname,
+					debug: false,
+					minimize: true,
+					sourceMap: false,
+					output: {
+						comments: false
+					},
+					compress: {
+						warnings: false,
+						screw_ie8: true
+					},
+				})
+			);
+			plugins.push(new ZipPlugin({ path: 'zip', filename: DEPLOY_FILE_NAME }));
+		}
+
+	})(argv)
+};
